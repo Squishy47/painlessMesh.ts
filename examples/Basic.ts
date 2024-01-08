19d38cecc99a95b9edcf675dfd9a9b4df604b28a
squishy <Squishy47@users.noreply.github.com>
2024-01-08 19:08:50 +0000

    added examples folder

---
+++ examples/Basic.ts
@@ -0,0 +1,13 @@
+import { LogLevel } from "../lib/Logger.ts";
+import Mesh from "../lib/Mesh.ts";
+
+const mesh = new Mesh({
+    host: "10.80.0.1",
+    port: 5555,
+});
+
+await mesh.start();
+
+mesh.on("broadcast", (data) => {
+    console.log(data);
+});
