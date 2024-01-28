8587106468c4e45d4bc8c30f2e96c1ae14c6e340
squishy <Squishy47@users.noreply.github.com>
2024-01-28 21:27:58 +0000

    added more call back examples. start no longer async

--- examples/Basic.ts
+++ examples/Basic.ts
@@ -1,12 +1,25 @@
+import { LogLevel } from "../lib/Logger.ts";
 import Mesh from "../lib/Mesh.ts";
 
 const mesh = new Mesh({
-    host: "10.80.0.1",
-    port: 5555,
+    host: "10.176.216.1",
+    logLevels: [LogLevel.ERROR, LogLevel.STARTUP, LogLevel.MSG_TYPES],
 });
 
-await mesh.start();
+mesh.start();
+
+mesh.on("single", (data) => {
+    console.log(data);
+});
 
 mesh.on("broadcast", (data) => {
     console.log(data);
+});
+
+mesh.on("connected", () => {
+    console.log("callback mesh connected");
+});
+
+mesh.on("disconnected", () => {
+    console.log("callback mesh disconnected");
 });
